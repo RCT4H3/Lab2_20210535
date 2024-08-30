ed24bada44dce0510257c1ccd4d0307b35004635
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2024-08-29 20:12:32 -0500

    segundo avance

--- Lab2_20210535/src/main/java/org/example/lab2_20210535/controller/controllerGame.java
+++ Lab2_20210535/src/main/java/org/example/lab2_20210535/controller/controllerGame.java
@@ -1,15 +1,25 @@
 package org.example.lab2_20210535.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class controllerGame {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/formConfig", method = RequestMethod.GET)
     public String paginaConfig(){
         return "formularioConfiguracion";
+    }
+
+    @PostMapping("/form/Config")
+    public String formConfig(@RequestParam("tamTarjeta") int tamTarjeta,
+                             @RequestParam("numTarjetas") int numTarjetas){
+        formarTarjetas(numTarjetas, tamTarjeta);
+        return "juegoBingo";
+    }
+
+    public void formarTarjetas(int numTarjetas, int tamTarjeta){
+        System.out.println("XD"+tamTarjeta);
+        System.out.println("AJAJA"+numTarjetas);
     }
 }
