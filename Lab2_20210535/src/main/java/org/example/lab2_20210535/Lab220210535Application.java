f7398a10b9842e6f28079c3ff1468a75cedb633b
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2024-08-29 19:28:35 -0500

    Nuevo proyecto del Lab2

---
+++ Lab2_20210535/src/main/java/org/example/lab2_20210535/Lab220210535Application.java
@@ -0,0 +1,13 @@
+package org.example.lab2_20210535;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab220210535Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab220210535Application.class, args);
+    }
+
+}
---
+++ Lab2_20210535/src/test/java/org/example/lab2_20210535/Lab220210535ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab2_20210535;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220210535ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
