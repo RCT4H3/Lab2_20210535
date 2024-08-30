923be4a52d36da147a4916df0432c28e9f94c0c9
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2024-08-29 20:53:34 -0500

    Avance 3

--- Lab2_20210535/src/main/java/org/example/lab2_20210535/controller/controllerGame.java
+++ Lab2_20210535/src/main/java/org/example/lab2_20210535/controller/controllerGame.java
@@ -2,24 +2,28 @@ package org.example.lab2_20210535.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 @Controller
 public class controllerGame {
 
-    @RequestMapping(value = "/formConfig", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String paginaConfig(){
         return "formularioConfiguracion";
     }
 
-    @PostMapping("/form/Config")
+    @PostMapping("/juegoBingo")
+    @GetMapping("/juegoBingo")
     public String formConfig(@RequestParam("tamTarjeta") int tamTarjeta,
-                             @RequestParam("numTarjetas") int numTarjetas){
-        formarTarjetas(numTarjetas, tamTarjeta);
+                             @RequestParam("numTarjetas") int numTarjetas, Model model){
+        model.addAttribute("tamTarjeta", tamTarjeta);
+        int[] lista= new int[tamTarjeta*tamTarjeta];
+        for (int pos = 0; pos < tamTarjeta*tamTarjeta; pos++){
+            lista[pos] = (int)(Math.random()*99+1);
+        }
+        model.addAttribute("lista", lista);
         return "juegoBingo";
     }
 
-    public void formarTarjetas(int numTarjetas, int tamTarjeta){
-        System.out.println("XD"+tamTarjeta);
-        System.out.println("AJAJA"+numTarjetas);
-    }
+
 }
